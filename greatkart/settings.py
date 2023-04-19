9479b808d009c51add1de15a983f4bfb7c223c45
musthaq0551 <x22137335@student.ncirl.ie>
2023-04-19 13:02:54 +0000

    static1

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'greatkart/static',
+]
 
 
 MEDIA_URL = '/media/'
