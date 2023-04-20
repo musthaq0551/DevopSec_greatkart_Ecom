0d0dde7a1acbae07c688d754366cd58d8c80e819
musthaq0551 <x22137335@student.ncirl.ie>
2023-04-20 17:48:06 +0000

    pychache cleared and made changes in settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -127,11 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS =[ 
-    'greatkart/static',
- ]
+STATIC_ROOT = 'static'
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS =[ 
+#     'greatkart/static',
+#  ]
 
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
