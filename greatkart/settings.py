3b2e6548a4d365016b9201fc06e0be8c6e8fa39d
musthaq0551 <x22137335@student.ncirl.ie>
2023-04-20 18:49:38 +0000

    new changes made

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -124,21 +124,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-# STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS =[ 
-#     'greatkart/static',
-#  ]
-
 STATIC_URL = '/static/'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
