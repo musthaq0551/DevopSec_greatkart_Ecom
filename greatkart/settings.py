b63889c26b4f7d24349bd04cf69c8261492b6022
musthaq0551 <x22137335@student.ncirl.ie>
2023-04-21 16:19:39 +0000

    settings.staticfiles

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'greatkart', 'static/')]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media/')
 MEDIA_URL = '/media/'
